64e3f0166acda91212d185aa94a0857433d64725
BlackMorgot <trololohost2@gmail.com>
2018-02-18 20:04:16 +0700

    The conclusion of a chessboard is realized

--- main.cpp
+++ main.cpp
@@ -1,4 +1,16 @@
 #include <iostream>
-
-int main() {
+ 
+int main()
+{
+    const int N = 8;
+    bool flag = true;
+    for (int i = 0; i < N * N; i++)
+    {
+        if (i % N == 0)
+        {
+            std::cout << '\n';
+            flag = !flag;
+        }
+        std::cout << (i & 1 ^ flag ? '*' : ' ');
+    }
 }
