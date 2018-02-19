cbdda751e434069c53cf8680751173fad94a8fb3
BlackMorgot <trololohost2@gmail.com>
2018-02-19 08:51:06 +0700

    Fix main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
- 
-int main()
-{
-    const int N = 8;
-    bool flag = true;
-    for (int i = 0; i < N * N; i++)
-    {
-        if (i % N == 0)
-        {
-            std::cout << '\n';
-            flag = !flag;
-        }
-        std::cout << (i && 1 ^ flag ? '*' : ' ');
-    }
+ using namespace std;
+int main() {
+cout << "r n b q k b n r" <<endl;
+cout << "p p p p p p p p" << endl;
+cout << "* * * * * * * *" << endl;
+cout << "* * * * * * * *" << endl;
+cout << "* * * * * * * *" << endl;
+cout << "* * * * * * * *" << endl;
+cout << "P P P P P P P P" << endl;
+cout << "R N B Q K B N R" <<endl;
 }
+
+
+   
